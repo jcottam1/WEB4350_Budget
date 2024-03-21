bd1b620b87c43653fbb97c0f2b8daf7d92de6955
Sharell Cook <sharellcook@mail.weber.edu>
2024-03-21 14:31:41 -0600

    Working on deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 #STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
 
 # Default primary key field type
