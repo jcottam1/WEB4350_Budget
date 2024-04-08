03350bc11766056101eae5abc448c4223ddd0e25
Michael090909 <23mikelattin@gmail.com>
2024-04-08 10:59:29 -0600

    attempting to fix issue with migration

--- budget/models.py
+++ budget/models.py
@@ -47,7 +47,7 @@ class BudgetLabel(models.Model):
     received = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     due_date = models.DateField()
     notes = models.CharField(max_length=100, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.label
